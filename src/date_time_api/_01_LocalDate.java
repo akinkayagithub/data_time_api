862b6e6f3f9ef6bae66c3cefef901f676e1c4c25
akinkayagithub <akinkaya.b@gmail.com>
2022-12-09 14:21:29 -0600

    added LocalTime basics

--- src/date_time_api/_01_LocalDate.java
+++ src/date_time_api/_01_LocalDate.java
@@ -11,7 +11,7 @@ public class _01_LocalDate {
         //Creating a LocalDate instance with now() method
         LocalDate localDate1 = LocalDate.now();
 
-        System.out.println(localDate1); // 2022-12-09
+        System.out.println(localDate1); // current date in the format of yyyy-MM-dd
 
         //Creating a LocalDate instance with of() method
         LocalDate localDate2 = LocalDate.of(2020, Month.APRIL, 4);
@@ -21,12 +21,12 @@ public class _01_LocalDate {
         //Adding n days to the current date
         LocalDate localDate3 = LocalDate.now().plusDays(5);
 
-        System.out.println(localDate3); // 2022-12-14
+        System.out.println(localDate3); // current date + 5 days
 
         //Subtracting n years from the current date using minus() method and ChronoUnit enumeration
         LocalDate localDate4 = LocalDate.now().minus(2, ChronoUnit.YEARS);
 
-        System.out.println(localDate4); // 2020-12-09
+        System.out.println(localDate4); // current date - 2 years
 
         //Get day, month and year of the LocalDate
         LocalDate localDate5 = LocalDate.parse("2000-03-15");
