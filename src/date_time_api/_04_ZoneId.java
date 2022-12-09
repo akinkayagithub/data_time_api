1d7916ef64df4c513bed67ac32a2b91b8da41d96
akinkayagithub <akinkaya.b@gmail.com>
2022-12-09 14:51:31 -0600

    added ZoneId basics

---
+++ src/date_time_api/_04_ZoneId.java
@@ -0,0 +1,18 @@
+package date_time_api;
+
+import java.time.ZoneId;
+
+public class _04_ZoneId {
+    public static void main(String[] args) {
+
+        //Creating a ZoneId instance with systemDefault() method
+        ZoneId zoneId1 = ZoneId.systemDefault();
+
+        System.out.println(zoneId1); // system default zone id
+
+        //Creating a ZoneId instance with of() method
+        ZoneId zoneId2 = ZoneId.of("America/Los_Angeles");
+
+        System.out.println(zoneId2); // America/Los_Angeles
+    }
+}
